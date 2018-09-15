3a3e71a3d5e37afb3e9e0d9ec63b3b046b2fb01e
Lê Minh Vinh <mvinhle@outlook.com>
2018-09-15 13:09:41 +0700

    Đã add resources, add 3 activity cần thiết và design thô main.

---
+++ app/src/main/java/tieuaa/mv/ami/AmiActivity.java
@@ -0,0 +1,13 @@
+package tieuaa.mv.ami;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AmiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ami);
+    }
+}
